306809423c61f25eb43754da1f17b1253a091273
Sammy <sammykumardemo@gmail.com>
2024-11-26 15:48:52 +0530

    added

--- src/streamApi/Test2.java
+++ src/streamApi/Test2.java
@@ -1,6 +1,9 @@
 package streamApi;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Test2 {
     public static void main(String[] args) {
@@ -16,5 +19,31 @@ public class Test2 {
                 System.out.println(i);
             }
         }
+
+        System.out.println("----Using Stream Api (first part) -----");
+        Stream<Integer> s1 = al.stream();
+        List<Integer> l = s1.filter(e -> e % 2 == 0).collect(Collectors.toList());
+        System.out.println(l);
+
+        System.out.println("----Using Stream api (Second part) -----");
+        Stream<Integer> s2 = al.stream();
+        s2.filter(e -> e % 2 == 0).forEach(e -> System.out.println(e));
+
+        System.out.println("----Using Stream api (Third part) -----");
+        al.stream().filter(e -> e % 2 == 0).forEach(e -> System.out.println(e));
+
+        System.out.println("----Count Even Numbers-----");
+        System.out.println(al.stream().filter(e -> e % 2 == 0).count());
+
+        System.out.println("----Method Chaining-----");
+        String name = "deepak";
+        String str1 = name.concat("Java");
+        String str2 = str1.toUpperCase();
+        int l1 = str2.length();
+        System.out.println(l1);
+
+        int leng = name.concat("Java").toUpperCase().length();
+        System.out.println(leng);
+
     }
 }

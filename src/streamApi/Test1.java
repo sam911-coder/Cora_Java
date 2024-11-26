3a55b2df6bc60e1122dfb1fb5c6378fc53a1e31e
Sammy <sammykumardemo@gmail.com>
2024-11-26 14:55:56 +0530

    added

--- src/streamApi/Test1.java
+++ src/streamApi/Test1.java
@@ -1,6 +1,8 @@
 package streamApi;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Test1 {
@@ -15,5 +17,35 @@ public class Test1 {
         // 1st Way to get stream instance
         Stream<Integer> s1 = al.stream();
         s1.forEach(e -> System.out.println(e));
+
+        System.out.println("----------");
+
+        // 2nd Way to get stream instance
+        Stream s2 = Stream.of(al);
+        s2.forEach(e -> System.out.println(e));
+
+        System.out.println("----------");
+
+        // 3rd Way to get stream instance
+        //  Stream<Integer> s3 = Stream.of(100, 200, 300, 400, 500);
+        Stream<String> s3 = Stream.of("deepak", "rahul", "amit", "deepesh", "Kamal");
+        s3.forEach(e -> System.out.println(e));
+
+        System.out.println("----------");
+
+        // 4t Way to get stream instance
+        int[] arr = {600, 700, 800, 900, 1000};
+        IntStream s4 = Arrays.stream(arr);
+        s4.forEach(e -> System.out.println(e));
+
+        System.out.println("----------");
+
+        // 5th Way to get stream instance
+        Stream<Object> s5 = Stream.empty();
+
+        System.out.println("----------");
+
+        // 6th Way to get stream instance
+        Stream<Object> s6 = Stream.builder().build();
     }
 }

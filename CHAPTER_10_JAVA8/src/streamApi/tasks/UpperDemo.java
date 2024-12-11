386a3a65bbec44bb9336a58a8e8dcdc4ce60c640
Sammy <sammykumardemo@gmail.com>
2024-12-11 15:20:27 +0530

    added

--- CHAPTER_10_JAVA8/src/streamApi/tasks/UpperDemo.java
+++ CHAPTER_10_JAVA8/src/streamApi/tasks/UpperDemo.java
@@ -7,8 +7,17 @@ public class UpperDemo {
     public static void main(String[] args) {
         List<String> fruits =
                 Arrays.asList("apple", "orange", "pineapple", "berry", "strawberry", "kiwi", "berry", "kiwi");
+
+        // with lambda expression
+        fruits.stream()
+                .map(str->str.toUpperCase())
+                .sorted()
+                .forEach(str-> System.out.println(str));
+
+        // with method reference
         fruits.stream()
                 .map(String::toUpperCase)
+                .sorted()
                 .forEach(System.out::println);
 
     }
